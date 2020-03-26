87052f4476f2d99c5888f88ec2e2c83cb303399c
Tristan Bilot <tristan_bilot@carrefour.com>
2020-03-26 11:25:38 +0100

    feat(lexer): lexer structure + comments

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -1,42 +1,41 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-#define MAX_TOKEN 256
 #include "lexer/token.h"
 
 /**
-** \brief This is your struct lexer forward declaration.
+** \brief Lexer architecture and methods.
 **
-** You should not edit this struct declaration but define one with the
-** same name in another header file.
+** \param input the full input string.
+** \param token_list the linked-list of tokens.
+** \param current the current token pointer in the list.
 */
 struct lexer {
     const char *input;
-    int index;
-    int length;
-    struct token *token_list[MAX_TOKEN];
+    struct token *token_list;
+    struct token *current;
 };
 
 /**
-** \brief Lexer allocator and initialiser
+** \brief Allocate and init a new lexer.
 ** \param str the string to use as input stream.
 */
-struct lexer *lexer_alloc(const char *str);
+struct lexer *new_lexer(const char *str);
 
 /**
-** \brief Wrapper to release every resources still held in a lexer.
+** \brief Free all ressources allocated in the lexer.
 **
-** \param lexer the lexer to free
+** \param lexer the lexer to free.
 */
-void lexer_free(struct lexer *lexer);
+void free_lexer(struct lexer *lexer);
 
 /**
-** \brief Return the first token from the input stream without consuming it.
+** \brief Return the next token without consume it.
 **
 ** \return the next token from the input stream
 ** \param lexer the lexer to lex from
 */
-struct token *lexer_peek(struct lexer *lexer);
+struct token *peek(struct lexer *lexer);
 
 /**
 ** \brief Return and consume the next token from the input stream.
@@ -44,6 +43,6 @@ struct token *lexer_peek(struct lexer *lexer);
 ** \return the next token from the input stream
 ** \param lexer the lexer to lex from
 */
-struct token *lexer_pop(struct lexer *lexer);
+struct token *pop(struct lexer *lexer);
 
 #endif /* ! LEXER_H */
--- tests/tests_lexer.c
+++ tests/tests_lexer.c
@@ -1,6 +1,8 @@
 #include <criterion/criterion.h>
+#include <token.h>
 
 Test(lexer, test) {
     const char *input = "";
-    struct lexer *lexer = new_lexer();
+    struct lexer *lexer = new_lexer(input);
+    cr_assert(lexer_pop(lexer) == TOK_EOF);
 }
