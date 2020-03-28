38c93ce4db4f0ce1a26786f4770cd85e594a4baf
enzar.salemi <enzar.salemi@epitech.eu>
2020-03-28 05:22:09 +0100

    adding option parsing

---
+++ src/main.c
@@ -0,0 +1,64 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <errno.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "my_42sh.h"
+
+void print_usage()
+{
+    fprintf(stderr, USAGE);
+    exit(1);
+}
+
+static struct option_sh *init_option_sh()
+{
+    struct option_sh *option = malloc(sizeof(struct option_sh));
+    if (option == NULL)
+    {
+        fprintf(stderr, "%s", strerror(errno));
+        exit(1);
+    }
+    option->norc_flag = false;
+    option->print_ast_flag = false;
+    option->cmd = NULL;
+    return option;
+}
+
+int main(int ac, char **av)
+{
+    int option_index = 0;
+    int opt = -1;
+    struct option_sh *option = init_option_sh();
+    static struct option long_options[] =
+    {
+        {"c", required_argument, 0, 'c'},
+        {"ast-printer", no_argument, 0, 'a'},
+        {"norc", no_argument, 0, 'n'},
+        {0, 0, 0, 0}
+    };
+    while ((opt = getopt_long(ac, av, "nac:", long_options, &option_index)) == -1)
+    {
+        if (opt == 'n')
+            option->norc_flag = true;
+        else if (opt == 'a')
+            option->print_ast_flag = true;
+        else if (opt == 'c')
+            option->cmd = optarg;
+        else if (opt == '?') {
+            if (optopt == 'c')
+                fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+            else if (isprint(optopt))
+                fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+            else
+                fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
+            print_usage();
+        }
+        else
+            print_usage();
+    }
+}
---
+++ src/my_42sh.h
@@ -0,0 +1,13 @@
+#ifndef MY_42SH_H
+#define MY_42SH_H
+
+#define USAGE "alors il faut mettre le usage là mais c casse couille"
+
+struct option_sh
+{
+    bool norc_flag;
+    bool print_ast_flag;
+    char *cmd;
+};
+
+#endif /* ! MY_42SH_H */
