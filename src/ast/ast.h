910a90343555fb21e7a80d216800bcd194b69cf2
Madushan THAMBITHURAI <madushan.thambithurai@epita.fr>
2020-03-28 18:33:00 +0100

    Modification des fichiers ast et parser et move des fichiers

--- src/ast/ast.h
+++ src/ast/ast.h
@@ -24,6 +24,49 @@ struct ast
     } data;
 };
 
+struct ast_node_compound_list
+{
+};
+
+struct  ast_node_if                              // INSTANCE KEYWORD
+{
+    struct ast_node_compound_list *condition;   // the condition
+    struct ast_node_compound_list *if_body;     // the body of the if
+    struct ast_node_else_clause *else_clause;   // the body of the else, may be NULL
+};
+
+struct ast_node_cmd                             // INSTANCE COMMAND
+{
+};
+
+struct ast_node_logical                         //INSTANCE LOGICAL
+{
+};
+
+struct ast_node_word
+{
+};
+
+struct ast_node_bracket
+{
+};
+
+struct ast_node_parenthesis
+{
+};
+
+struct ast_node_if
+{
+};
+
+struct ast_node_for
+{
+};
+
+struct ast_node_while
+{
+};
+
 static inline struct ast *left_child(struct ast *ast)
 {
     return ast->data.children.left;
--- src/ast/ast_implement.c
+++ src/ast/ast_implement.c
@@ -27,45 +27,8 @@ struct ast_node *ast_node_alloc(void)
         return NULL;
     return res;
 }
-
 void ast_node_free(struct ast_node *node)
 {
     if(node != NULL)
         free(node);
 }
-
-struct ast_node_compound_list
-{
-    struct ast_node *compound;
-};
-
-struct ast_node_if                              // INSTANCE KEYWORD
-{
-    struct ast_node_compound_list *condition;   // the condition
-    struct ast_node_compound_list *if_body;     // the body of the if
-    struct ast_node_else_clause *else_clause;   // the body of the else, may be NULL
-};
-struct ast_node_else_clause
-struct ast_node_command                         // INSTANCE COMMAND
-{
-    struct ast_node_compound_list *commands;
-};
-
-struct ast_node_logical                         //INSTANCE LOGICAL
-{
-    struct ast_node_compound_list *logicals;
-};
-
-struct ast_node_word 
-{
-    struct ast_node_compound_list *word;
-};
-/*struct ast *ast_alloc_number(int value)
-{
-    struct ast *res = ast_alloc();
-    res->type = EXPR_NUMBER;
-    res->data.value = value;
-    return res;
-}*/
-
-
