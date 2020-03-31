03bb9c7ba4790f0a096e02f587db350198c95241
Tristan Bilot <tristan_bilot@carrefour.com>
2020-03-31 19:14:59 +0200

    feat(parser): some tests

--- tests/tests_parser.c
+++ tests/tests_parser.c
@@ -6,98 +6,84 @@
 
 Test(parser, parse_simple_command){
     //one simple command
-    struct lexer *lexer = new_lexer("ls");
-    struct parser *parser = init_parser(lexer);
+    struct parser *parser = init_parser(new_lexer("ls"));
     void *ast = NULL;
     cr_assert(!parse_input(parser, ast));
-    /*
+
     // simple command with param
-    struct lexer *lexer = new_lexer("echo test");
-    struct parser *parser = init_parser(lexer);
-    struct node_input *ast = NULL;
-    cr_assert(!parse_input(parser, ast));
+    struct parser *parser2 = init_parser(new_lexer("echo test"));
+    void *ast2 = NULL;
+    cr_assert(!parse_input(parser2, ast2));
 
     // simple command with assigment word
-    struct lexer *lexer = new_lexer("a=1 echo a");
-    struct parser *parser = init_parser(lexer);
-    struct node_input *ast = NULL;
-    cr_assert(!parse_input(parser, ast));
-    */
+    struct parser *parser3 = init_parser(new_lexer("a=1 echo a"));
+    void *ast3 = NULL;
+    cr_assert(!parse_input(parser3, ast3));
 }
 /*
 Test(parser, parse_simple_if){
     // simple if
-    struct lexer *lexer = new_lexer("if a then b fi");
-    struct parser *parser = init_parser(lexer);
-    struct node_input *ast = NULL;
+    struct parser *parser = init_parser(new_lexer("if a then b fi"));
+    void *ast = NULL;
     cr_assert(!parse_input(parser, ast));
     
     // if else
-    struct lexer *lexer = new_lexer("if a then b else c fi");
-    struct parser *parser = init_parser(lexer);
-    struct node_input *ast = NULL;
-    cr_assert(!parse_input(parser, ast));
+    struct parser *parser2 = init_parser(new_lexer("if a then b else c fi"));
+    void *ast2 = NULL;
+    cr_assert(!parse_input(parser2, ast2));
     
     // if elif else
-    struct lexer *lexer = new_lexer("if a then b elif c else d fi");
-    struct parser *parser = init_parser(lexer);
-    struct node_input *ast = NULL;
-    cr_assert(!parse_input(parser, ast));
+    struct parser *parser3 = init_parser(new_lexer("if a then b elif c else d fi"));
+    void *ast3 = NULL;
+    cr_assert(!parse_input(parser3, ast3));
 }
 
 Test(parser, parser_and_or_simple){
-    struct lexer *lexer = new_lexer("ls | cat test");
-    struct parser *parser = init_parser(lexer);
+    struct parser *parser = init_parser(new_lexer("ls | cat test"));
+    void *ast = NULL;
     cr_assert(!parse_input(parser, ast));
 
-    struct lexer *lexer = new_lexer("ls || cat test ");
-    struct parser *parser = init_parser(lexer);
-    cr_assert(!parse_input(parser, ast));
+    struct parser *parser2 = init_parser(init_parser(new_lexer("ls || cat test")););
+    void *ast2 = NULL;
+    cr_assert(!parse_input(parser2, ast2));
 
-    struct lexer *lexer = new_lexer("ls & cat Makefile");
-    struct parser *parser = init_parser(lexer);
-    cr_assert(!parse_input(parser, ast));
+    struct parser *parser3 = init_parser(new_lexer("ls & cat Makefile"));
+    void *ast3 = NULL;
+    cr_assert(!parse_input(parser3, ast3));
 
-    struct lexer *lexer = new_lexer("ls && cat Makefile");
-    struct parser *parser = init_parser(lexer);
-    cr_assert(!parse_input(parser, ast));
+    struct parser *parser4 = init_parser(new_lexer("ls && cat Makefile"));
+    void *ast4 = NULL;
+    cr_assert(!parse_input(parser4, ast4));
     
 }
 
-Test(parser, parser_){
-    struct lexer *lexer = new_lexer("/n");
-    struct parser *parser = init_parser(lexer);
-    cr_assert(!parse_input(parser, ast));
-}
-Test(parser, parser_){
-    struct lexer *lexer = new_lexer(" ");
-    struct parser *parser = init_parser(lexer);
-    cr_assert(!parse_input(parser, ast));
-}
-Test(parser, parser_){
-    struct lexer *lexer = new_lexer("ls;echo toto");
-    struct parser *parser = init_parser(lexer);
-    cr_assert(!parse_input(parser, ast));
-}
-Test(parser, parser_){
-    struct lexer *lexer = new_lexer("ls | echo test | cat test");
-    struct parser *parser = init_parser(lexer);
-    cr_assert(!parse_input(parser, ast));
-}
-Test(parser, parser_){
-    struct lexer *lexer = new_lexer("ls || echo test || cat test");
-    struct parser *parser = init_parser(lexer);
-    cr_assert(!parse_input(parser, ast));
-}
+Test(parser, parser_multi_logical){
 
-Test(parser, parser_){
-    struct lexer *lexer = new_lexer("ls & echo test & cat test");
-    struct parser *parser = init_parser(lexer);
+    struct parser *parser = init_parser(new_lexer("ls | echo test || cat test"));
+    void *ast = NULL;
     cr_assert(!parse_input(parser, ast));
+
+    struct parser *parser2= init_parser(new_lexer("ls || echo test || cat test"));
+    void *ast2 = NULL;
+    cr_assert(!parse_input(parser2, ast2));
+
+    struct parser *parser3 = init_parser(new_lexer("ls && echo test && | cat test"));
+    void *ast3 = NULL;
+    cr_assert(!parse_input(parser3, ast3));
+
 }
 Test(parser, parser_){
-    struct lexer *lexer = new_lexer("ls && echo test && cat test");
-    struct parser *parser = init_parser(lexer);
+
+     struct parser *parser = init_parser(new_lexer("/n"));
+    void *ast = NULL;
     cr_assert(!parse_input(parser, ast));
+ 
+    struct parser *parser2 = init_parser(new_lexer(" "));
+    void *ast2 = NULL;
+    cr_assert(!parse_input(parser2, ast2));
+
+    struct parser *parser3 = init_parser(new_lexer("ls;echo toto"));
+    void *ast3 = NULL;
+    cr_assert(!parse_input(parser3, ast3));
 }
 */
